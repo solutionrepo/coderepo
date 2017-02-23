a1272a8f0b2f8b9925afa3442b4e8bb35c1fbfae
Shantanu Singh <shantanu@gohelper.in>
2017-02-23 23:30:31 +0530

    --Factorial

---
+++ Basics of Programming/factorial.c
@@ -0,0 +1,22 @@
+// Author - Shantanu9
+// Problem - basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/find-factorial/
+// C
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    unsigned long long factorial = 1;
+
+    scanf("%d",&n);
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else
+    {
+      for(i=1; i<=n; ++i)
+        {
+            factorial *= i;
+        }
+        printf("%llu", factorial);
+    }
+    return 0;
+}
